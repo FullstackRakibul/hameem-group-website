a15240a9dc3f7679bf78d85023892e8b461498b4
FullstackRakibul <95490833+FullstackRakibul@users.noreply.github.com>
2025-01-25 08:32:55 +0600

    office pre code

--- components/Header.vue
+++ components/Header.vue
@@ -1,176 +1,144 @@
 <!-- <header class="border-b-2 bg-white border-indigo-100 fixed top-0 left-0 right-0 z-10 page-container"> -->
 <template>
-<header
+  <header :class="[
+    'fixed top-0 left-0 right-0 z-10 page-container transition-all duration-300',
+    isScrolled ? ' shadow-lg' : 'bg-transparent'
+  ]">
+    <div class="fixed top-0 left-0 right-0 z-10 container mx-auto transition-all duration-300">
+      <nav class="mx-auto sm:px-6 lg:px-8">
+        <div class="flex items-center justify-between h-16">
+          <!-- Logo -->
+          <div class="flex-shrink-0 flex items-center">
+            <NuxtLink to="/" class="flex">
+              <img src="/public/assets/group-logo.png" alt="Hameem Group Logo" class="h-8 w-auto" />
 
-:class="[
-      'fixed top-0 left-0 right-0 bg-slate-500 z-10 page-container transition-all duration-300',
-      isScrolled ? 'bg-white shadow-lg' : 'bg-transparent'
-    ]"
->
-  <div class="fixed top-0 left-0 right-0 z-10 container mx-auto transition-all duration-300" >
-    <nav class="mx-auto sm:px-6 lg:px-8">
-      <div class="flex items-center justify-between h-16">
-        <!-- Logo -->
-        <div class="flex-shrink-0 flex items-center">
-           <NuxtLink to="/" class="flex">
-             <img src="/public/assets/group-logo.png" alt="Hameem Group Logo" class="h-8 w-auto" />
-            
-           </NuxtLink>
-        </div>
+            </NuxtLink>
+          </div>
 
-        <!-- Desktop Menu -->
-         <div class="flex items-center justify-between">
+          <!-- Desktop Menu -->
+          <div class="flex items-center justify-between">
 
-           <div class="hidden md:flex space-x-2 px-3">
-             <NuxtLink to="/" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md">
-               Home
-             </NuxtLink>
-             <NuxtLink to="/about" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md f">
-               About Us
-             </NuxtLink>
-             <NuxtLink to="/#business-units" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md">
-               Business Units
-             </NuxtLink>
-             <NuxtLink to="/#sustainability" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md ">
-               Subtainability
-             </NuxtLink>
-             <NuxtLink to="/#industries" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md ">
-               Products
-             </NuxtLink>
-             <NuxtLink to="/#clients" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md "">
+            <div class="hidden md:flex space-x-2 px-3">
+              <NuxtLink to="/"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md">
+                Home
+              </NuxtLink>
+              <NuxtLink to="/about"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md f">
+                About Us
+              </NuxtLink>
+              <NuxtLink to="/#business-units"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md">
+                Business Units
+              </NuxtLink>
+              <NuxtLink to="/#sustainability"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md ">
+                Subtainability
+              </NuxtLink>
+              <NuxtLink to="/#industries"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md ">
+                Products
+              </NuxtLink>
+              <NuxtLink to="/#clients"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md "">
                Clients
              </NuxtLink>
-             <NuxtLink to="/#gallary" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md=">
-               Gallary
-             </NuxtLink>
-             <NuxtLink to="/contact" class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md">
-               Contact Us
-             </NuxtLink>
-           </div>
-   
-           <!-- Download Brochure Button -->
+             <NuxtLink to=" /#gallary"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md=">
+                Gallary
+              </NuxtLink>
+              <NuxtLink to="/contact"
+                class="text-white font-semibold hover:text-primary hover:bg-slate-200 hover:rounded-full px-3 py-2 text-md">
+                Contact Us
+              </NuxtLink>
+            </div>
+
+            <!-- Download Brochure Button -->
             <el-space>
-              <Search/>
+              <Search />
               <div class="hidden md:block">
-                <a
-                  href="https://www.linkedin.com/company/hameemgroup"
-                  target="_blank"
-                  download
-                  class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-[#264156] hover:bg-[#5C2D23]"
-                >
+                <a href="https://www.linkedin.com/company/hameemgroup" target="_blank" download
+                  class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-[#264156] hover:bg-[#5C2D23]">
                   Career
                 </a>
               </div>
             </el-space>
-           <LanguageSwitcher/>
-         </div>
+            <LanguageSwitcher />
+          </div>
 
-        <!-- Mobile Menu Button -->
-        <div class="md:hidden">
-          <button
-            @click="isMenuOpen = true"
-            class="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-blue-600 focus:outline-none"
-            aria-label="Open main menu"
-          >
-            <MenuIcon class="h-6 w-6" />
-          </button>
-        </div>
-      </div>
-    </nav>
-
-    <!-- Mobile Menu Overlay -->
-    <Transition name="fade">
-      <div v-if="isMenuOpen" class="fixed inset-0 z-50 ">
-        <div class="absolute inset-0 bg-black opacity-50" @click="isMenuOpen = false"></div>
-        <div class="absolute top-0 right-0 w-3/4 max-w-xs bg-white shadow-lg h-full p-4 overflow-y-auto">
-          <div class="flex items-center justify-between mb-8">
-            <div class="flex items-center">
-              <img src="/public//assets/group-logo.png" alt="Hameem Group Logo" class="h-8 w-auto" />
-              <span class="ml-2 text-xl font-bold text-gray-800">Ha-Meem Group</span>
-            </div>
-            <button
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 focus:outline-none"
-              aria-label="Close menu"
-            >
-              <CloseIcon class="h-6 w-6" />
+          <!-- Mobile Menu Button -->
+          <div class="md:hidden">
+            <button @click="isMenuOpen = true"
+              class="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-blue-600 focus:outline-none"
+              aria-label="Open main menu">
+              <MenuIcon class="h-6 w-6" />
             </button>
           </div>
-          <nav class="flex flex-col space-y-4">
-            <NuxtLink
-              to="/"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              Home
-            </NuxtLink>
-            <NuxtLink
-              to="/about"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              About Us
-            </NuxtLink>
-            <NuxtLink
-              to="/"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              Business Units
-            </NuxtLink>
-            <NuxtLink
-              to="/"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              Subtainability
-            </NuxtLink>
-            <NuxtLink
-              to="/"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              Products
-            </NuxtLink>
-            <NuxtLink
-              to="/sister-concerns/refat-garments-ltd"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              Clients
-            </NuxtLink>
-            <NuxtLink
-              to="/sister-concerns/refat-garments-ltd"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              Gallary
-            </NuxtLink>
-            
-            <NuxtLink
-              to="/contact"
-              @click="isMenuOpen = false"
-              class="text-gray-700 hover:text-blue-600 text-lg font-medium"
-            >
-              Contact Us
-            </NuxtLink>
-            <el-space>
-                 <div class="hidden md:block">
-                   <a
-                     href="https://www.linkedin.com/company/hameemgroup"
-                     target="_blank"
-                     download
-                     class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-[#264156] hover:bg-[#5C2D23]"
-                   >
-                     Career
-                   </a>
-                 </div>
-               </el-space>
-          </nav>
         </div>
-      </div>
-    </Transition>
-  </div>
+      </nav>
+
+      <!-- Mobile Menu Overlay -->
+      <Transition name="fade">
+        <div v-if="isMenuOpen" class="fixed inset-0 z-50 ">
+          <div class="absolute inset-0 bg-black opacity-50" @click="isMenuOpen = false"></div>
+          <div class="absolute top-0 right-0 w-3/4 max-w-xs bg-white shadow-lg h-full p-4 overflow-y-auto">
+            <div class="flex items-center justify-between mb-8">
+              <div class="flex items-center">
+                <img src="/public//assets/group-logo.png" alt="Hameem Group Logo" class="h-8 w-auto" />
+                <span class="ml-2 text-xl font-bold text-gray-800">Ha-Meem Group</span>
+              </div>
+              <button @click="isMenuOpen = false" class="text-gray-700 hover:text-blue-600 focus:outline-none"
+                aria-label="Close menu">
+                <CloseIcon class="h-6 w-6" />
+              </button>
+            </div>
+            <nav class="flex flex-col space-y-4">
+              <NuxtLink to="/" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                Home
+              </NuxtLink>
+              <NuxtLink to="/about" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                About Us
+              </NuxtLink>
+              <NuxtLink to="/" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                Business Units
+              </NuxtLink>
+              <NuxtLink to="/" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                Subtainability
+              </NuxtLink>
+              <NuxtLink to="/" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                Products
+              </NuxtLink>
+              <NuxtLink to="/sister-concerns/refat-garments-ltd" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                Clients
+              </NuxtLink>
+              <NuxtLink to="/sister-concerns/refat-garments-ltd" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                Gallary
+              </NuxtLink>
+
+              <NuxtLink to="/contact" @click="isMenuOpen = false"
+                class="text-gray-700 hover:text-blue-600 text-lg font-medium">
+                Contact Us
+              </NuxtLink>
+              <el-space>
+                <div class="hidden md:block">
+                  <a href="https://www.linkedin.com/company/hameemgroup" target="_blank" download
+                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-white bg-[#264156] hover:bg-[#5C2D23]">
+                    Career
+                  </a>
+                </div>
+              </el-space>
+            </nav>
+          </div>
+        </div>
+      </Transition>
+    </div>
   </header>
 </template>
 
@@ -200,10 +168,13 @@ const isMenuOpen = ref(false)
 
 <style scoped>
 /* Fade Transition for Mobile Menu */
-.fade-enter-active, .fade-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 0.3s;
 }
-.fade-enter-from, .fade-leave-to {
+
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
 }
 </style>
