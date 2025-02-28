5bb4e7b1b6c77d4b1c9dece00cec2f0fe158a7c8
fullstackrakibul <rakibul.cse.47@gmail.com>
2025-03-01 04:08:32 +0600

    client added

---
+++ components/denim/RFIDPromoVideoSection.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container mx-auto">
+    <el-row>
+      <el-col :span="8" class="p-6 bg-gray-50">
+        part 01
+      </el-col>
+      <el-col :span="16" class="p-6 bg-gray-50">
+        part 02
+      </el-col>
+    </el-row>
+  </section>
+</template>
--- pages/denim/index.vue
+++ pages/denim/index.vue
@@ -2,15 +2,19 @@
 import DenimSlider01 from '~/components/v2/index/sliders/DenimSlider01.vue';
 import HameemAtaGlance from '~/components/denim/HameemAtaGlance.vue';
 import DenimStickySection from '~/components/v2/ui/DenimStickySection.vue';
+import RFIDPromoVideoSection from '~/components/denim/RFIDPromoVideoSection.vue';
+import OurClientSection from '~/components/v2/index/OurClientSection.vue';
 </script>
 <template>
   <section>
     <DenimSlider01 />
     <HameemAtaGlance />
+    <RFIDPromoVideoSection/>
     <DenimStickySection
       bgImage="https://api.hameemgroup.com:9012/Resources/hameem-group-website/denim-stickyBanner-hameem-group.jpeg"
       mainText="Our Denim Journey"
       subText="WHO DON'T HAVE TO TRY TOO HARD"
     />
+    <OurClientSection/>
   </section>
 </template>
